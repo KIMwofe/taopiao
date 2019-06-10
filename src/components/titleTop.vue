39db56c2811bc8678579f61cf18c7d2b6d39b850
KIMwofe <245902043@qq.com>
2019-06-10 21:12:58 +0800

    不知道做了些什么

---
+++ src/components/titleTop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <van-tabs v-model="active" line-width="15">
+    <van-tab title="正在上映" disabled color="black">地址</van-tab>
+    <van-tab title="正在上映" >正在上映</van-tab>
+    <van-tab title="正在上映">即将上映</van-tab>
+  </van-tabs>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      active: 2
+    }
+  }
+}
+</script>
+<style lang="less">
+
+</style>
--- src/views/index/film.vue
+++ src/views/index/film.vue
@@ -1,5 +1,56 @@
 <template>
   <div>
+        <titleTop/>
     shouye
   </div>
 </template>
+<script>
+import titleTop from '@/components/titleTop.vue'
+export default {
+
+  components: {
+    titleTop
+  }
+}
+</script>
+<style lang="less">
+  .top{
+    display: flex;
+    flex-wrap: nowrap;
+  .city-c{
+    display: inline-block;
+    vertical-align: top;
+    height: 3.125rem;
+    height: 13.3vw;
+    line-height: 3.125rem;
+    line-height: 13.3vw;
+    .current-city{
+        background-size: 25px 25px;
+        background-repeat: no-repeat;
+        background-position: 15px;
+        padding-left: 50px;
+        position: relative;
+    }
+    .i-logo{
+      background-image: url(//gw.alicdn.com/tfs/TB1mKkzl9zqK1RjSZFpXXakSXXa-50-50.svg);
+    }
+    span{
+      margin-left: 3px;
+      width: 8px;
+      height: 4px;
+      position: relative;
+       top: 50%;
+      left: 65%;
+      width: 0;
+      height: 0;
+      border-left: 4px solid transparent;
+      border-right: 4px solid transparent;
+      border-top: 4px solid #333;
+      display: block;
+      position: absolute;
+      margin: -1px 0 0 -4px;
+    }
+  }
+
+  }
+</style>
